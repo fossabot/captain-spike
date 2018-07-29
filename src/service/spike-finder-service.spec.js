968bcb74f1ddebbde9e1c720b50cc688739f096d
jaedle <dennis.jekubczyk@gmail.com>
2018-07-29 19:53:49 +0200

    Started implementing spike finder service.

---
+++ src/service/spike-finder-service.spec.js
@@ -0,0 +1,8 @@
+describe('SpikeFinderService', () => {
+  describe('findSpikes', () => {
+    it('should return found spikes', () => {
+      expect(true)
+        .toBe(false);
+    });
+  });
+});
