3af0ed77e0d5009cd539ef601c3d82c32ed1c0ff
jaedle <dennis.jekubczyk@gmail.com>
2018-08-03 20:10:57 +0200

    chore: given version for sonar-scanner

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
-
 npx semantic-release
-version="$(git describe --tags `git rev-list --tags --max-count=1` | sed 's/v//g')"
+published_version="$(git describe --tags \"$(git rev-list --tags --max-count=1)\" | sed 's/v//g')"
+
 docker image tag jaedle/captain-spike:latest jaedle/captain-spike:${version}
 docker push jaedle/captain-spike:latest
-docker push jaedle/captain-spike:${version}
+docker push jaedle/captain-spike:${published_version}
+
+sonar-scanner -Dsonar.projectVersion="$published_version"
